183766efb8f4aef4219790b53f2c0a557268472a
FabulousBernt <johnny.vigersten@icloud.com>
2025-12-03 19:50:16 +0100

    fixed port bug

--- main.go
+++ main.go
@@ -33,7 +33,8 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("PORT environment variable is not set")
+		log.Println("PORT not set, defaulting to 8080")
+		port = "8080"
 	}
 
 	apiCfg := apiConfig{}
